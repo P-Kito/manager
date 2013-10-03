a4743380915c8e23999680fb5f0907f7704ad013
Kito <kito@vortexirc.com>
2013-10-03 19:57:24 +0200

    Edit footer fail

--- modules/home.mod.php
+++ modules/home.mod.php
@@ -2,7 +2,7 @@
 $news = TextMgr::getText('wichtige_news', true);
 if ($news != "") echo "<h4>".$news."</h4>";
 ?>
-<h1>Willkommen im StraMa!</h1>
+<h1>Willkommen im Strafen Manager Laenalith!</h1>
 
 <br />
 <br />
