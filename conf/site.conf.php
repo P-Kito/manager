995d003531b3a3efc7c5ac9b8ad42d08112f5e9d
Kito <kito@vortexirc.com>
2013-10-03 21:31:48 +0200

    Edit text manager implement db

--- conf/site.conf.php
+++ conf/site.conf.php
@@ -4,7 +4,7 @@ class CONFIG {
 // MySQL Verbindungsdaten
 const USERNAME = "verwarn";
 const PASSWORD = "test";
-const HOSTNAME = "127.0.0.1:3306";
+const HOSTNAME = "127.0.0.1";
 const DB1 = 'character';
 const DB2 = 'verwarn_manager';
 
