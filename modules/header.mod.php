1d388c055d376a5667a59b778708c3e021913fb9
Kito <kito@vortexirc.com>
2013-10-03 22:05:15 +0200

    Clean

--- modules/header.mod.php
+++ modules/header.mod.php
@@ -17,6 +17,7 @@
 <div id='cssmenu'>
 <ul>
    <li <?php if ($p == "home") echo "class=\"active\""; ?>><a href='index.php?p=home'><span><?php echo TextMgr::getText('menu_home', false); ?></span></a></li>
+   <li <?php if ($p == "newissue") echo "class=\"active\""; ?>><a href='index.php?p=newissue'><span><?php echo TextMgr::getText('menu_newissue', false); ?></span></a></li>
 </ul>
 </div>
 </div>
