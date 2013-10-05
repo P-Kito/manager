9e1e8e7f30220af886ee244660ef0f0961b5be0e
Kito <kito@vortexirc.com>
2013-10-05 21:10:28 +0200

    Account MGR

--- includes/MySQLMgr.class.php
+++ includes/MySQLMgr.class.php
@@ -6,29 +6,29 @@
  
 class MySQLMgr
 {
-	static $c1 = null; // Auth
-	static $c2 = null; // General Website
+	static $authDB = null; // Auth
+	static $webDB = null; // General Website
 	
 	static function connectDB($user, $pass, $host)
 	{
 		// Used for Auth
-		self::$c1 = mysql_connect($host,$user,$pass);
+		self::$authDB = mysql_connect($host,$user,$pass);
 		// Used for general website
-		self::$c2 = mysql_connect($host,$user,$pass);
+		self::$webDB = mysql_connect($host,$user,$pass);
 	}
 	
 	static function selectDB($db1, $db2)
 	{
-		mysql_select_db($db1, self::$c1);
-		mysql_select_db($db2, self::$c2);
+		mysql_select_db($db1, self::$authDB);
+		mysql_select_db($db2, self::$webDB);
 	}
 	
 	static function executeSingle($query, $db = false /* always auth db*/)
 	{
 		if ($db)
-			$result = mysql_query($query, self::$c2);
+			$result = mysql_query($query, self::$webDB);
 		else
-			$result = mysql_query($query, self::$c1);
+			$result = mysql_query($query, self::$authDB);
 		$row = mysql_fetch_array($result);
 		return($row[0]);
 	}
@@ -36,24 +36,24 @@ class MySQLMgr
 	static function executeUpdate($query, $db = false)
 	{
 		if ($db) 
-			$result = mysql_query($query, self::$c2);
+			$result = mysql_query($query, self::$webDB);
 		else
-			$result = mysql_query($query, self::$c1);
+			$result = mysql_query($query, self::$authDB);
 	}
 	
 	static function executeMulti($query, $db = false)
 	{
 		if ($db) 
-			$result = mysql_query($query, self::$c2);
+			$result = mysql_query($query, self::$webDB);
 		else
-			$result = mysql_query($query, self::$c1);
+			$result = mysql_query($query, self::$authDB);
 		return($result);
 	}
 	
 	static function closeDB ()
 	{
-		mysql_close(self::$c1);
-		mysql_close(self::$c2);
+		mysql_close(self::$authDB);
+		mysql_close(self::$webDB);
 	}
 }
 ?>
