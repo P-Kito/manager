11e6fda0d83de03a75c65187fb78fd77939ca88b
Kito <kito@vortexirc.com>
2013-10-05 21:54:06 +0200

    Account MGR

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -11,8 +11,6 @@ class AccountMgr
 		$query = "SELECT id FROM account WHERE username='".$username."'";
 		$result = MySQLMgr::executeSingle($query, false);
 		if (!$result)
-			die(TextMgr::getText('internal_error', false));
-		if ($result == "")
 			return(false);
 		else
 			return(true);
