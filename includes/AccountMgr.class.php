da7120f4483634c200426d9c9d61f0396211bd38
Kito <kito@vortexirc.com>
2013-10-05 21:02:19 +0200

    Account MGR

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -9,7 +9,7 @@ class AccountMgr
 	static function checkExist($username)
 	{
 		$query = "SELECT id FROM account WHERE username='".$username."'";
-		$result = MySQLMgr::executeSingle($query, false);
+		$result = MySQLMgr::executeSingle($query);
 		if ($result != "")
 			return true;
 		else
