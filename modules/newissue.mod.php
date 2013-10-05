04cd020fa8bf8e01bec172e3910b0e34276a29a5
Kito <kito@vortexirc.com>
2013-10-05 20:27:28 +0200

    Init newissue site

--- modules/newissue.mod.php
+++ modules/newissue.mod.php
@@ -1,16 +1,19 @@
-<hr />
-<?php echo TextMgr::getText('new_issue', false); ?>
-<hr />
+<?php
+echo TextMgr::getText('new_issue', false);
 
-    <form class="form">  
-      
-        <p class="name">  
-            <input type="text" name="name" id="name" />  
-            <label for="name">Accountname</label>  
-        </p>  
-      
-        <p class="submit">  
-            <input type="submit" value="Suchen" />  
-        </p>  
-      
-    </form>  
+if (isset($_POST["send"]))
+{
+} else {
+?>
+<form class="form" action="<?php echo $_SERVER['PHP_SELF'].'?p=newissue'; ?>" method="post" id="searchacc">
+    <p class="name">  
+        <input type="text" name="name" id="name" />
+        <label for="name">Accountname</label>
+    </p>
+    <p class="submit">  
+        <input type="submit" name="send" value="Suchen" />  
+    </p> 
+</form>
+<?php
+}
+?>
