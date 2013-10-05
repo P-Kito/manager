f98212c0272d3d0076424aefeee0d036e25d0b7c
Kito <kito@vortexirc.com>
2013-10-06 00:15:29 +0200

    Add search

--- modules/search.mod.php
+++ modules/search.mod.php
@@ -7,6 +7,7 @@ if (isset($_POST["send"]))
 		$accdata = AccountMgr::fetchData($username);
 		$accdata = mysql_fetch_array($accdata);
 		/* [0] = GUID */
+		echo "<meta http-equiv=\"refresh\" content=\"0.5; URL=" . $_SERVER['PHP_SELF'].'?p=search&guid=' . $accdata[0] ."\">";
 		echo TextMgr::getText('case_header_search', false, true, array($username));
 		echo "
 		<table cellspacing='0'>
