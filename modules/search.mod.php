9364b747e8c45143229b40a6b3e5e9b7365e7fa6
Kito <kito@vortexirc.com>
2013-10-06 00:08:35 +0200

    Add search

---
+++ modules/search.mod.php
@@ -0,0 +1,43 @@
+<?php
+if (isset($_POST["send"]))
+{
+	$username = mysql_real_escape_string($_POST["name"]);
+	if (AccountMgr::checkExist($username))
+	{
+		$accdata = AccountMgr::fetchData($username);
+		$accdata = mysql_fetch_array($accdata);
+		/* [0] = GUID */
+		echo TextMgr::getText('case_header', false, true, array($username));
+		echo "
+		<table cellspacing='0'>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Verwarnstufe</th>
+					<th>Kommentar</th>
+					<th>Datum</th>
+				</tr>
+			</thead>
+			<tbody>
+			".AccountMgr::getHistory($accdata[0])."
+			</tbody>
+		</table>
+		";
+	}
+	else
+		echo TextMgr::getText('character_not_found', false);
+} else {
+echo TextMgr::getText('new_issue', false);
+?>
+<form class="form" action="<?php echo $_SERVER['PHP_SELF'].'?p=newissue'; ?>" method="post" id="searchacc">
+    <p class="name">  
+        <input type="text" name="name" id="name" />
+        <label for="name">Accountname</label>
+    </p>
+    <p class="submit">  
+        <input type="submit" name="send" value="<?php echo TextMgr::getText('account_search', false); ?>" />  
+    </p> 
+</form>
+<?php
+}
+?>
