ce8cfeaab1df93d3d1298423a8817da12cc9254a
Kito <kito@vortexirc.com>
2013-10-06 00:41:44 +0200

    Add search

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -28,7 +28,7 @@ class AccountMgr
 		$result = MySQLMgr::executeMulti($query, false);
 		$html = "";
 		$wirkung = split(';', $text);
-		if (!$result)
+		if (!$row = mysql_fetch_arry($result))
 			$html .= "<tr><td>--</td><td>--</td><td>--</td><td>--</td></tr>";
 		else
 			while ($row = mysql_fetch_row($result))
