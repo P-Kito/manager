3969d2c663195ae563397eaeffd15337bec42a8c
Kito <kito@vortexirc.com>
2013-10-05 19:32:46 +0200

    Init newissue site

--- modules/newissue.mod.php
+++ modules/newissue.mod.php
@@ -1,12 +1,11 @@
 <hr />
 <?php echo TextMgr::getText('new_issue', false); ?>
 <hr />
-<br />
     <form class="form">  
       
         <p class="name">  
             <input type="text" name="name" id="name" />  
-            <label for="name">Name</label>  
+            <label for="name">Charaktername</label>  
         </p>  
       
         <p class="email">  
