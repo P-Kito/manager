570fbee18534613b4ab1e00360006f4207f68848
Kito <kito@vortexirc.com>
2014-02-07 19:39:34 +0100

    third push new system

--- modules/home.mod.php
+++ modules/home.mod.php
@@ -2,6 +2,7 @@
 $news = TextMgr::getText('wichtige_news', true);
 if ($news != "") echo "<h4>".$news."</h4>";
 ?>
+
 <h1>Willkommen im Strafen Manager Laenalith!</h1>
 
 <?php echo TextMgr::getText('home_introduction', false); ?>
