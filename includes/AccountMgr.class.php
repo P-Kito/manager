901df0c4cee6951864a0030ec43c1c3816d306e4
Kito <kito@vortexirc.com>
2014-02-09 11:26:27 +0100

    show creator and color it to admin/gm color

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -146,7 +146,7 @@ class AccountMgr
 			$html .= "<td>" . $row[1] . "</td>";
 			$html .= "<td>" . $wirkung[$row[2]-1] . "</td>";
 			$html .= "<td>" . $row[3] . "</td>";
-			$html .= "<td>" . ucfirst(self::getUsernameByStramaAccID($row[4])) . "</td>";
+			$html .= "<td><font color=\"".TextMgr::getText($_SESSION['rank'], false)."\">" . ucfirst(self::getUsernameByStramaAccID($row[4])) . "</font></td>";
 			$html .= "<td>" . $row[5] . "</td>";
 			$html .= "</tr>";
 		}
