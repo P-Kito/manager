ccc27e9e811412a1c32b5017f74f67ca2044d34a
Kito <kito@vortexirc.com>
2014-02-10 12:24:19 +0100

    test out new style

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -58,7 +58,7 @@ class AccountMgr
 				$html .= "<td><font color=\"".TextMgr::getText(self::getStramaAccRank($row[4]), false)."\">" . ucfirst(self::getUsernameByStramaAccID($row[4])) . "</font></td>";
 				$html .= "</tr>";
 			} else {
-				$html .= "<tr id=\"alt\">";
+				$html .= "<tr class=\"alt\">";
 				$html .= "<td>" . $row[0] . "</td>";
 				$html .= "<td>" . $wirkung[$row[1]-1] . "</td>";
 				$html .= "<td>" . $row[2] . "</td>";
