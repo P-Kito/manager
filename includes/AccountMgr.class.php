ce5da16dd01fe1510ba34d4d48e8fcb4416c5709
Kito <kito@vortexirc.com>
2014-02-10 12:42:01 +0100

    test out new style

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -58,8 +58,8 @@ class AccountMgr
 				$html .= "<td><font color=\"".TextMgr::getText(self::getStramaAccRank($row[4]), false)."\">" . ucfirst(self::getUsernameByStramaAccID($row[4])) . "</font></td>";
 				$html .= "</tr>";
 			} else {
-				$html .= "<tr>";
-				$html .= "<td><img src=\"images/warning.gif\"></td>";
+				$html .= "<tr style=\"background-image: url('images/warning.gif');\">";
+				$html .= "<td>". $row[0] . "</td>";
 				$html .= "<td>" . $wirkung[$row[1]-1] . "</td>";
 				$html .= "<td>" . $row[2] . "</td>";
 				$html .= "<td>" . $row[3] . "</td>";
