6be28c0794b5189d89d71fd5f80f75af3217acd2
Kito <kito@vortexirc.com>
2014-02-10 14:27:42 +0100

    Finalize cosmetic and backend shit

--- modules/newissue.mod.php
+++ modules/newissue.mod.php
@@ -24,6 +24,7 @@ if (!isset($_SESSION['login']))
 		$accdata = AccountMgr::fetchData($username);
 		$accdata = mysql_fetch_array($accdata);
 		/* [0] = GUID */
+		echo "<meta http-equiv=\"refresh\" content=\"0.5; URL=" . $_SERVER['PHP_SELF'].'?p=newissue&guid=' . $accdata[0] ."\">";
 		echo TextMgr::getText('case_header', false, true, array($username));
 		echo "
 		<form class=\"form\" action=\"".$_SERVER['PHP_SELF'].'?p=newissue&ban=ok&guid='.$accdata[0].''."\" method=\"post\" id=\"editacc\">
