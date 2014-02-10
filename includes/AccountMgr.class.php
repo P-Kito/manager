9695bae452a179313e570ed9f8d7b08936519fe7
Kito <kito@vortexirc.com>
2014-02-10 13:35:51 +0100

    test out new style

--- includes/AccountMgr.class.php
+++ includes/AccountMgr.class.php
@@ -62,7 +62,7 @@ class AccountMgr
 			} else {
 				$html .= "<tr>";
 				$html .= "<td>" . $row[0] . "</td>";
-				$html .= "<td class=\"info\"><img src=\"images/warning.gif\"></td>";
+				$html .= "<td><img src=\"images/warning.gif\"></td>";
 				$html .= "<td>" . $wirkung[$row[1]-1] . "</td>";
 				$html .= "<td>" . $row[2] . "</td>";
 				$html .= "<td>" . $row[3] . "</td>";
